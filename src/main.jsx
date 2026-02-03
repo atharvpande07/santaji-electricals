3cb2ae8e775601dbc160a25af340f53e1082375d
Atharv Pande <atharvpande2427@gmail.com>
2026-02-03 15:30:13 +0530

    Fix routing and environment variables for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ initializeGTM();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
