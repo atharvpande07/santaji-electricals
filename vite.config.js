7e79ffbbbbb841791160db240b626911d05d68f5
Atharv Pande <atharvpande2427@gmail.com>
2026-03-05 11:39:25 +0530

    Fix GitHub Pages 404 natively using 404.html trick

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            name: 'copy-index-to-404',
+            closeBundle() {
+                fs.copyFileSync('dist/index.html', 'dist/404.html')
+            }
+        }
+    ],
     base: '/',
 })
